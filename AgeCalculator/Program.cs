398211a5cbfc205bdb7a18dbee798a311f508f6b
Summertime Fred <summertimefred@outlook.com>
2020-12-14 19:32:16 -0500

    Add project files.

---
+++ AgeCalculator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //parse()
+            string strYear = "2001";
+            int year = Int32.Parse(strYear);
+
+            Console.WriteLine("The year is:" + year);
+
+
+            //System.Date
+
+            int currentYear = System.DateTime.Now.Year;
+
+            Console.WriteLine("The current year is:" + currentYear);
+
+        }
+    }
+}
